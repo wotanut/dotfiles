1c5537a48bd1d1e1f34983ee4ebe0bae47db3510
wotanut <sam@sambot.dev>
2024-09-13 15:22:55 +0100

    Added basic wilder config

--- .config/nvim/lua/plugins/wilder.lua
+++ .config/nvim/lua/plugins/wilder.lua
@@ -1,6 +1,20 @@
 return {
- 'gelguy/wilder.nvim',
-  config = function ()
-    require("wilder").setup()
-  end
+  "gelguy/wilder.nvim",
+  config = function()
+    require("wilder").setup {
+      modes = { ":", "/", "?" },
+    }
+    local wilder = require "wilder"
+    wilder.set_option(
+      "renderer",
+      wilder.popupmenu_renderer(wilder.popupmenu_border_theme {
+        highlights = {
+          border = "Normal", -- highlight to use for the border
+        },
+        -- 'single', 'double', 'rounded' or 'solid'
+        -- can also be a list of 8 characters, see :h wilder#popupmenu_border_theme() for more details
+        border = "rounded",
+      })
+    )
+  end,
 }
