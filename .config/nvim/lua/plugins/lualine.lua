37fddcb1374e5bd981d60086259658cfe8fe389e
wotanut <sam@sambot.dev>
2024-09-13 15:35:02 +0100

    Added lualine

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,16 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { "encoding", "fileformat", "filetype" },
+        lualine_y = { "hostname" },
+        lualine_z = { "location" },
+      },
+    }
+  end,
+}
