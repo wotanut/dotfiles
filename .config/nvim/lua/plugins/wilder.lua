1d529570132946fe5ee760714f7fd63357a09689
wotanut <sam@sambot.dev>
2024-09-13 15:24:49 +0100

    Re-organised file

--- .config/nvim/lua/plugins/wilder.lua
+++ .config/nvim/lua/plugins/wilder.lua
@@ -1,10 +1,8 @@
 return {
   "gelguy/wilder.nvim",
   config = function()
-    require("wilder").setup {
-      modes = { ":", "/", "?" },
-    }
     local wilder = require "wilder"
+    wilder.setup { modes = { ":", "/", "?" } }
     wilder.set_option(
       "renderer",
       wilder.popupmenu_renderer(wilder.popupmenu_border_theme {
