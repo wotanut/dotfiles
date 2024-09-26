754eedfe3ee7f89798c3dfe3d84b1589ba9d13fb
wotanut <sam@sambot.dev>
2024-09-26 14:21:14 +0100

    Added session plugin + Added neo-tree autoreveal

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,13 +1,29 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    "3rd/image.nvim",
+  },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = true,
+        show_hidden_count = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        hide_by_name = {
+          ".git",
+          ".DS_Store",
+          "thumbs.db",
+        },
+        never_show = {},
+      },
     },
     config = function()
-	vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
-    end
+      vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left<CR>", {})
+    end,
+  },
 }
